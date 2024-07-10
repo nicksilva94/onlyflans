d05a831460972f912291be4fcaeb470c2cdc37b0
Nico <nsilvaf94@gmail.com>
2024-07-09 22:00:41 -0400

    Creacion web y urls

--- onlyflans/onlyflans/urls.py
+++ onlyflans/onlyflans/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from web.views import indice, acerca, bienvenido
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indice, name="indice"),
+    path('acerca', acerca, name="acerca"),
+    path('bienvenido', bienvenido, name="bienvenido"),
 ]
---
+++ onlyflans/web/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def indice(request):
+    return render(request, 'index.html', {})
+
+
+def acerca(request):
+    return render(request, 'about.html', {})
+
+
+def bienvenido(request):
+    return render(request, 'welcome.html', {})
+# Create your views here.
